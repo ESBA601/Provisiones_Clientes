82e44d0acd738071ca2e5088ac610f55ee145480
ESBA601 <erich_18@hotmail.com>
2024-09-07 09:24:44 -0430

    Actualizacion Informacion, Se actualiza dos fechas, la informacion con el archivo la fecha del proximo cierre y la otra fecha que busca el archivo del dia que se esta ejecutando.

---
+++ Actualiza_Informacion.R
@@ -0,0 +1,46 @@
+# COLOCAR LA FECHA DE ESTUDIO
+
+date0 <- as.character(230918)
+
+# COLOCAR LA RUTA DE LA INFORMACION
+
+rc <- "C:/Users/ERITK S. BASTOS/Desktop/Insumos/ATRASO/"
+
+# COMIENZA A CONSTRUIR LA RUTA
+
+r0 <- gsub(" ","", paste('IBSRM14FTB_',date0,'.TXT'))
+
+# SE AJUSTA LA RUTA PARA PODER LEER
+
+rf0 <- gsub("/ ","/", paste(rc,r0))
+
+# LEER EL TXT
+
+tod0 <- read.delim(rf0, header = FALSE, sep = "©", dec = ",")
+
+# COLOCAR LA FECHA DEL PROXIMO CIERRE
+
+FECHA1 <- as.Date("2023-08-31","%Y-%m-%d")
+
+FECHA2 <- data.frame(FECHA1) 
+
+CIERRE <- rename(FECHA2, c("CIERRE"="FECHA1"))
+
+# CALCULAR LA ESTIMACION DE DIAS AL CIERRE 
+
+# SEPARAR LOS DIAS, MESES Y AÑOS
+
+da <- str_sub(date0, 1, 2)
+dm <- str_sub(date0, 3, 4)
+dd <- str_sub(date0, 5, 6)
+
+# FORMAR LA FECHA CON EL FORMATO DE LA FECHA
+
+FECHA <- paste(20,da,"-",dm,"-",dd)
+FECHA <- gsub(" ","",FECHA)
+FECHA <- as.Date(FECHA,"%Y-%m-%d")
+
+# CALCULAR LOS DIAS ESTIMADOS AL CIERRE
+
+ds <- as.numeric(FECHA1-FECHA)
+
